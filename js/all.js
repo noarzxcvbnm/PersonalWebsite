399eb0dbb7cd41296735b8e23fbb6d8a3b0104e6
noarzxcvbnm <68560915+noarzxcvbnm@users.noreply.github.com>
2021-09-04 01:12:32 +0800

    ScrollHeader

--- js/all.js
+++ js/all.js
@@ -7,12 +7,23 @@ $('.navbar-toggler').click(function () {
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
+  // console.log("currentScrollPos:"+currentScrollPos)
+  // console.log("prevScrollpos:"+prevScrollpos)
+  
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
+    if (document.getElementById("header") != null){
+      document.getElementById("header").style.top = "0";
+    }
+    if (document.getElementById("footer") != null){
     document.getElementById("footer").style.bottom = "0";
+    }
   } else {
+    if (document.getElementById("header") != null){
     document.getElementById("header").style.top = "-90px";
+    }
+    if (document.getElementById("footer") != null){
     document.getElementById("footer").style.bottom = "-60px";
+    }
   }
   prevScrollpos = currentScrollPos;
 }
