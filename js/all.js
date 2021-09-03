fe6374c492da51941cb280197d2af0cd889f2721
noarzxcvbnm <68560915+noarzxcvbnm@users.noreply.github.com>
2021-09-03 12:06:43 +0800

    1.5

--- js/all.js
+++ js/all.js
@@ -1,3 +1,18 @@
 $('.navbar-toggler').click(function () {
     $('.navbar-nav').toggleClass('active')
   })
+
+
+  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+    document.getElementById("footer").style.bottom = "0";
+  } else {
+    document.getElementById("header").style.top = "-90px";
+    document.getElementById("footer").style.bottom = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
